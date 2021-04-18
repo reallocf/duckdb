c17e52795dde1c2945749836c5e0455009c3f804
Haneen Mohammed <hamohammed.sa@gmail.com>
2021-04-18 13:37:48 -0400

    add unittests for hash join/index join/groupby perfect and default aggregate

---
+++ test/sql/lineage/CMakeFiles/test_index.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haneen/Downloads/duckdb/test/sql/index/ub_test_index.cpp" "/home/haneen/Downloads/duckdb/test/sql/index/CMakeFiles/test_index.dir/ub_test_index.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DUCKDB_BUILD_LIBRARY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/include"
+  "third_party/fmt/include"
+  "third_party/hyperloglog"
+  "third_party/re2"
+  "third_party/miniz"
+  "third_party/utf8proc/include"
+  "third_party/miniparquet"
+  "third_party/concurrentqueue"
+  "third_party/pcg"
+  "third_party/tdigest"
+  "test/../third_party/catch"
+  "test/../third_party/tpce-tool/include"
+  "test/../third_party/sqlite/include"
+  "test/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/sql/lineage/CMakeFiles/test_index.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_index.dir/ub_test_index.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_index.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
