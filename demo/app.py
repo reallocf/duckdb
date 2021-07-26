6bdfac39f146f0371fa7baee4e285a3177402608
Charlie Summers <charlie@gomerits.com>
2021-07-26 17:15:15 -0600

    Add smoked duck demo flask app

---
+++ demo/app.py
@@ -0,0 +1,173 @@
+import duckdb
+from flask import Flask, request
+from flask_cors import CORS
+from threading import Lock
+
+app = Flask(__name__)
+CORS(app)
+
+# Keep track of expected values for each so we can make sure that zeros are set when a brushing would otherwise remove a bar
+expectedCauses = []
+expectedStates = []
+expectedYears = []
+
+con = duckdb.connect(database=":memory:", read_only=False)
+con.execute("CREATE TABLE fires AS SELECT * FROM read_csv_auto('data/fires_with_dropped_cols.csv', SAMPLE_SIZE=-1)")
+dbLock = Lock()
+
+def query(sql):
+  dbLock.acquire()
+  try:
+    res = con.execute(sql)
+  finally:
+    dbLock.release()
+  return res
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+def jsonifyDuckData(data):
+  return [{"x": d[0], "y": d[1]} for d in data]
+
+@app.route("/lineage", methods=["POST"])
+def toggleLineage():
+  req = request.json
+  toggle = req["toggle"]
+  # TODO is this right?
+  query(f"pragma LINEAGE={'ON' if toggle else 'OFF'}")
+
+@app.route("/sql", methods=["POST"])
+def executeSQL():
+  # Expects sql to be a list of sql statements of the form "SELECT foo, COUNT(*) FROM fires" or "SELECT bar, MAX(foo) FROM fires"
+  # or some lineage query that results in a map from x to y
+  req = request.json
+  sqls = req["sqls"]
+  res = {"res": {sql["name"]: jsonifyDuckData(query(sql["value"]).fetchall()) for sql in sqls}}
+  return res
+
+@app.route("/initial", methods=["POST"])
+def getInitialData():
+  global expectedCauses, expectedStates, expectedYears
+  causeData = jsonifyDuckData(query("SELECT STAT_CAUSE_DESCR, COUNT(*) FROM fires GROUP BY STAT_CAUSE_DESCR").fetchall())
+  expectedCauses = [d["x"] for d in causeData]
+  stateData = jsonifyDuckData(query("SELECT STATE, COUNT(*) FROM fires GROUP BY STATE").fetchall())
+  expectedStates = [d["x"] for d in stateData]
+  yearData = jsonifyDuckData(query("SELECT FIRE_YEAR, COUNT(*) FROM fires GROUP BY FIRE_YEAR").fetchall())
+  expectedYears = [d["x"] for d in yearData]
+  return {"cause": causeData, "state": stateData, "year": yearData}
+
+@app.route("/update", methods=["POST"])
+def getDataByBrushings():
+  global expectedCauses, expectedStates, expectedYears
+  if len(expectedCauses) == 0 or len(expectedStates) == 0 or len(expectedYears) == 0:
+    # hackiness...
+    raise RuntimeError("Must call /initial before /update")
+  req = request.json
+  whatChanged = req["changed"]
+  causeBrush = req["cause"]
+  stateBrush = req["state"]
+  yearBrush = req["year"]
+  yearBrush = [str(y) for y in yearBrush]
+  if whatChanged == "cause":
+    causeData = ""
+    stateData = updateState(causeBrush, yearBrush)
+    yearData = updateYear(causeBrush, stateBrush)
+  elif whatChanged == "state":
+    causeData = updateCause(stateBrush, yearBrush)
+    stateData = ""
+    yearData = updateYear(causeBrush, stateBrush)
+  elif whatChanged == "year":
+    causeData = updateCause(stateBrush, yearBrush)
+    stateData = updateState(causeBrush, yearBrush)
+    yearData = ""
+  else:
+    raise ValueError(f"Unexpected whatChanged {whatChanged}")
+  causeData = {cause[0]: cause[1] for cause in causeData}
+  causeData = [[cause, 0] if cause not in causeData else [cause, causeData[cause]] for cause in expectedCauses]
+  stateData = {state[0]: state[1] for state in stateData}
+  stateData = [[state, 0] if state not in stateData else [state, stateData[state]] for state in expectedStates]
+  yearData = {year[0]: year[1] for year in yearData}
+  yearData = [[year, 0] if year not in yearData else [year, yearData[year]] for year in expectedYears]
+  res = {"cause": jsonifyDuckData(causeData), "state": jsonifyDuckData(stateData), "year": jsonifyDuckData(yearData)}
+  return res
+
+# TODO replace all of the below with actual lineage queries
+def updateCause(stateBrush, yearBrush):
+  if len(stateBrush) != 0 and len(yearBrush) != 0:
+    q = f"""
+SELECT STAT_CAUSE_DESCR, COUNT(*)
+FROM fires
+WHERE STATE IN ('{"','".join(stateBrush)}') AND FIRE_YEAR IN ('{"','".join(yearBrush)}')
+GROUP BY STAT_CAUSE_DESCR
+    """
+  elif len(stateBrush) != 0:
+    q = f"""
+SELECT STAT_CAUSE_DESCR, COUNT(*)
+FROM fires
+WHERE STATE IN ('{"','".join(stateBrush)}')
+GROUP BY STAT_CAUSE_DESCR
+    """
+  elif len(yearBrush) != 0:
+    q = f"""
+SELECT STAT_CAUSE_DESCR, COUNT(*)
+FROM fires
+WHERE FIRE_YEAR IN ('{"','".join(yearBrush)}')
+GROUP BY STAT_CAUSE_DESCR
+    """
+  else:
+    q = "SELECT STAT_CAUSE_DESCR, COUNT(*) FROM fires GROUP BY STAT_CAUSE_DESCR"
+  return query(q).fetchall()
+
+def updateState(causeBrush, yearBrush):
+  if len(causeBrush) != 0 and len(yearBrush) != 0:
+    q = f"""
+SELECT STATE, COUNT(*)
+FROM fires
+WHERE STAT_CAUSE_DESCR IN ('{"','".join(causeBrush)}') AND FIRE_YEAR IN ('{"','".join(yearBrush)}')
+GROUP BY STATE
+    """
+  elif len(causeBrush) != 0:
+    q = f"""
+SELECT STATE, COUNT(*)
+FROM fires
+WHERE STAT_CAUSE_DESCR IN ('{"','".join(causeBrush)}')
+GROUP BY STATE
+    """
+  elif len(yearBrush) != 0:
+    q = f"""
+SELECT STATE, COUNT(*)
+FROM fires
+WHERE FIRE_YEAR IN ('{"','".join(yearBrush)}')
+GROUP BY STATE
+    """
+  else:
+    q = "SELECT STATE, COUNT(*) FROM fires GROUP BY STATE"
+  return query(q).fetchall()
+
+def updateYear(causeBrush, stateBrush):
+  if len(causeBrush) != 0 and len(stateBrush) != 0:
+    q = f"""
+SELECT FIRE_YEAR, COUNT(*)
+FROM fires
+WHERE STAT_CAUSE_DESCR IN ('{"','".join(causeBrush)}') AND STATE IN ('{"','".join(stateBrush)}')
+GROUP BY FIRE_YEAR
+    """
+  elif len(causeBrush) != 0:
+    q = f"""
+SELECT FIRE_YEAR, COUNT(*)
+FROM fires
+WHERE STAT_CAUSE_DESCR IN ('{"','".join(causeBrush)}')
+GROUP BY FIRE_YEAR
+    """
+  elif len(stateBrush) != 0:
+    q = f"""
+SELECT FIRE_YEAR, COUNT(*)
+FROM fires
+WHERE STATE IN ('{"','".join(stateBrush)}')
+GROUP BY FIRE_YEAR
+    """
+  else:
+    q = "SELECT FIRE_YEAR, COUNT(*) FROM fires GROUP BY FIRE_YEAR"
+  return query(q).fetchall()
+
