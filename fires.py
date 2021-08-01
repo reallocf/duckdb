4da8ff185e27ba9656b05170c2e9ed62dc37215d
Haneen Mohammed <hamohammed.sa@gmail.com>
2021-08-01 15:31:29 -0400

    remove join on hash_group_by_qid_opid_out since it is one-to-one mapping

--- fires.py
+++ fires.py
@@ -7,12 +7,15 @@ con.execute("CREATE TABLE fires AS SELECT * FROM read_csv_auto('/tmp/fires_with_
 
 # enable lineage
 con.execute("PRAGMA trace_lineage='ON'")
+con.execute("PRAGMA enable_profiling")
 Q1 = "SELECT STAT_CAUSE_DESCR, COUNT(*) FROM fires GROUP BY STAT_CAUSE_DESCR"
 Q2 = "SELECT STATE, COUNT(*) FROM fires GROUP BY STATE"
+#Q3 = "SELECT FIRE_YEAR, COUNT(*) FROM fires GROUP BY FIRE_YEAR"
 
 # run queries
 Q1_out = con.execute(Q1).fetchdf()
 Q2_out = con.execute(Q2).fetchdf()
+#Q3_out = con.execute(Q3).fetchdf()
 
 # disable lineage
 con.execute("PRAGMA trace_lineage='OFF'")
@@ -20,6 +23,7 @@ con.execute("PRAGMA trace_lineage='OFF'")
 # get queries' ids
 q1_id = con.execute("SELECT query_id FROM queries_list WHERE query='{}'".format(Q1)).fetchdf().iloc[0,0]
 q2_id = con.execute("SELECT query_id FROM queries_list WHERE query='{}'".format(Q2)).fetchdf().iloc[0,0]
+#q3_id = con.execute("SELECT query_id FROM queries_list WHERE query='{}'".format(Q3)).fetchdf().iloc[0,0]
 
 tables_list = con.execute("PRAGMA show_tables").fetchdf()
 print(tables_list)
@@ -28,7 +32,7 @@ def test_lineage_for_q(qid):
     '''
     This only supports selet group by query
     '''
-    con.execute("CREATE OR REPLACE VIEW lineage_{} AS SELECT sink.index AS input, out.index AS output, sink.chunk_id FROM HASH_GROUP_BY_{}_0_OUT AS out, HASH_GROUP_BY_{}_0_SINK AS sink WHERE out.value=sink.value".format(qid, qid, qid))
+    con.execute("CREATE OR REPLACE VIEW lineage_{} AS SELECT index AS input, value AS output, chunk_id FROM HASH_GROUP_BY_{}_0_SINK".format(qid, qid, qid))
     start = time.time()
     bw = con.execute("SELECT input+range_start as input, output FROM lineage_{}, seq_scan_{}_0_range AS r WHERE r.chunk_id=lineage_{}.chunk_id AND output=1".format(qid, qid, qid)).fetchdf()
     end = time.time()
